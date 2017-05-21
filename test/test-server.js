6a20c21d1bffa8b3d3e0aff2b527033cbe11f1e8
ljyockey <linyockey@gmail.com>
2017-05-20 18:23:49 -0700

    added tests and integrated with Travis/Heroku

---
+++ test/test-server.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+const should = chai.should();
+chai.use(chaiHttp);
+const app = server.app;
+const storage = server.storage;
+
+describe('index.html', function() {
+
+	it('should return HTML file', function() {
+		return chai.request(app)
+		.get('')
+		.then(function(res) {
+			res.should.have.status(200);
+			res.should.be.html;
+		})
+		.catch(function(err) {
+			console.log(err);
+		});
+	});
+});
